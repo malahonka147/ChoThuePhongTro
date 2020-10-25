00e21781e74367cf99c257471613552f51702832
Danger <Danger@DESKTOP-106RFMU>
2020-10-25 21:12:43 +0700

    ádfsa

--- DACSN/DACSN/ChiTietSanPham.aspx.cs
+++ DACSN/DACSN/ChiTietSanPham.aspx.cs
@@ -11,7 +11,7 @@ namespace DACSN
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
--- DACSN/DACSN/ChiTietSanPham.aspx.designer.cs
+++ DACSN/DACSN/ChiTietSanPham.aspx.designer.cs
@@ -11,41 +11,5 @@ namespace DACSN {
     
     
     public partial class ChiTietSanPham {
-        
-        /// <summary>
-        /// DropDownList4 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList DropDownList4;
-        
-        /// <summary>
-        /// DropDownList2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList DropDownList2;
-        
-        /// <summary>
-        /// DropDownList3 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList DropDownList3;
-        
-        /// <summary>
-        /// Button1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
